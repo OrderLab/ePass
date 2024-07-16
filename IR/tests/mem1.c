9ee774dd9c256f7287fc9b4bdbc9bb7155c54478
linsyking <kxiang@umich.edu>
2024-07-16 16:46:52 -0400

    feat: add nop

--- IR/tests/mem1.c
+++ IR/tests/mem1.c
@@ -9,7 +9,7 @@
 
 SEC("xdp")
 int prog(void *ctx) {
-    int id = bpf_ktime_get_ns() % 10;
+    int id = bpf_ktime_get_ns() % 20;
     int arr[10] = {};
     for (int i = 0; i < 10; ++i) {
         arr[i] = i;
