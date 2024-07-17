0abe50288ac6880006ddfbed5c0628d9d8dbdc80
linsyking <kxiang@umich.edu>
2024-07-17 00:13:27 -0400

    feat: add constraint pass

--- IR/add_constraint_pass.c
+++ IR/add_constraint_pass.c
@@ -7,4 +7,6 @@ void init_test_constraints(struct ir_function *fun) {
     fun->value_constraints = INIT_ARRAY(struct ir_constraint);
 }
 
-void add_constraint(struct ir_function *fun) {}
+void add_constraint(struct ir_function *fun) {
+    init_test_constraints(fun);
+}
--- IR/include/passes.h
+++ IR/include/passes.h
@@ -1,6 +1,7 @@
 #ifndef __BPF_IR_PASSES_H__
 #define __BPF_IR_PASSES_H__
 
+#include "add_constraint_pass.h"
 #include "phi_pass.h"
 #include "reachable_bb.h"
 #include "add_counter_pass.h"
@@ -10,7 +11,7 @@
     All function passes.
  */
 static void (*passes[])(struct ir_function *fun) = {
-    gen_reachable_bbs, remove_trivial_phi,
+    gen_reachable_bbs, remove_trivial_phi, add_constraint,
     // add_counter,
 };
 
