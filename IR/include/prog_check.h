24a9c7b0688e1504afab4e5880f1ccf5f7ca6ee8
linsyking <kxiang@umich.edu>
2024-07-24 11:55:43 -0400

    feat: check prog

---
+++ IR/include/prog_check.h
@@ -0,0 +1,8 @@
+#ifndef __BPF_IR_PROG_CHECK_H__
+#define __BPF_IR_PROG_CHECK_H__
+
+#include "ir_fun.h"
+
+void prog_check(struct ir_function *fun);
+
+#endif
---
+++ IR/prog_check.c
@@ -0,0 +1,6 @@
+#include "prog_check.h"
+
+// Check that the program is valid and able to be compiled
+void prog_check(struct ir_function *fun){
+    
+}
