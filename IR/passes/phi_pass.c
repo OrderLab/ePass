ecbbc9bb58987f3bd01888e9f8884937837bfad1
linsyking <kxiang@umich.edu>
2024-08-14 01:24:32 -0400

    fix: phi pass

--- IR/passes/phi_pass.c
+++ IR/passes/phi_pass.c
@@ -32,25 +32,8 @@ void try_remove_trivial_phi(struct ir_insn *phi) {
     if (!same_has_value) {
         same.type = IR_VALUE_UNDEF;
     }
-    struct ir_value phi_val;
-    phi_val.type        = IR_VALUE_INSN;
-    phi_val.data.insn_d = phi;
-    struct ir_insn **pos;
-    array_for(pos, phi->users) {
-        struct ir_insn *user = *pos;
-        if (user == phi) {
-            continue;
-        }
+    replace_all_usage_except(phi, same, phi);
 
-        struct array      value_uses = get_operands(user);
-        struct ir_value **pos2;
-        array_for(pos2, value_uses) {
-            if (ir_value_equal(**pos2, phi_val)) {
-                **pos2 = same;
-            }
-        }
-        array_free(&value_uses);
-    }
     erase_insn(phi);
 }
 
