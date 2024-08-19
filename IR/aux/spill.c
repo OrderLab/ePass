304fc6dd75b70efc007198636d5c3d28554f051a
linsyking <kxiang@umich.edu>
2024-08-19 15:31:52 -0400

    feat: spill draft

--- IR/aux/spill.c
+++ IR/aux/spill.c
@@ -3,15 +3,19 @@
 #include "dbg.h"
 #include "ir_insn.h"
 
+enum val_type vtype_insn(struct ir_insn *insn) {
+    insn                           = dst(insn);
+    struct ir_insn_cg_extra *extra = insn_cg(insn);
+    if (extra->spilled) {
+        return STACK;
+    } else {
+        return REG;
+    }
+}
+
 enum val_type vtype(struct ir_value val) {
     if (val.type == IR_VALUE_INSN) {
-        struct ir_insn          *insn  = dst(val.data.insn_d);
-        struct ir_insn_cg_extra *extra = insn_cg(insn);
-        if (extra->spilled) {
-            return STACK;
-        } else {
-            return REG;
-        }
+        return vtype_insn(val.data.insn_d);
     } else if (val.type == IR_VALUE_CONSTANT) {
         return CONST;
     } else if (val.type == IR_VALUE_STACK_PTR) {
@@ -19,6 +23,18 @@ enum val_type vtype(struct ir_value val) {
     } else {
         CRITICAL("No such value type for dst");
     }
+}
+
+void load_stack_to_vr(struct ir_insn *insn, struct ir_value *val, enum ir_vr_type ty) {
+    struct ir_insn *tmp = create_insn_base_cg(insn->parent_bb);
+    insert_at(tmp, insn, INSERT_FRONT);
+    tmp->op        = IR_INSN_LOAD;
+    tmp->value_num = 1;
+    tmp->values[0] = *val;
+    tmp->vr_type   = ty;
+
+    val->type        = IR_VALUE_INSN;
+    val->data.insn_d = tmp;
 }
 
 int check_need_spill(struct ir_function *fun) {
@@ -29,8 +45,8 @@ int check_need_spill(struct ir_function *fun) {
         struct ir_basic_block *bb = *pos;
         struct ir_insn        *insn;
         list_for_each_entry(insn, &bb->ir_insn_head, list_ptr) {
-            struct ir_value v0 = insn->values[0];
-            struct ir_value v1 = insn->values[1];
+            struct ir_value *v0 = &insn->values[0];
+            struct ir_value *v1 = &insn->values[1];
             if (insn->op == IR_INSN_ALLOC) {
                 // dst = alloc <size>
                 // Nothing to do
@@ -38,32 +54,57 @@ int check_need_spill(struct ir_function *fun) {
                 // store v0(dst) v1
                 // v0: reg ==> v1: reg, const, stack
                 // v0: stack ==> v1: reg, const
-                if (vtype(v0) == STACK && vtype(v1) == STACK) {
+                if (vtype(*v0) == STACK && vtype(*v1) == STACK) {
                     // Cannot directly copy stack value to stack
                     // Example:
                     // store s-8 s-16
                     // -->
                     // %tmp = load s-16
                     // store s-8 %tmp
-                    struct ir_insn *tmp = create_insn_base_cg(bb);
-                    insert_at(tmp, insn, INSERT_FRONT);
-                    tmp->op = IR_INSN_LOAD;
-                    tmp->value_num = 1;
-                    tmp->values[0] = 0;
-                    // No need to add users at this point
+
+                    // First get type of the dst
+                    enum ir_vr_type ty = v0->data.insn_d->vr_type;
+
+                    load_stack_to_vr(insn, v1, ty);
                 }
             } else if (insn->op == IR_INSN_LOAD) {
+                // OK
             } else if (insn->op == IR_INSN_LOADRAW) {
-            } else if (insn->op == IR_INSN_STORE) {
+                // OK
             } else if (insn->op == IR_INSN_STORERAW) {
+                // Built-in store instruction, OK
+            } else if (insn->op >= IR_INSN_ADD && insn->op < IR_INSN_CALL) {
+                // Binary ALU
+                // add reg reg
+                // add const reg
+                // There should be no stack
+                if (vtype(*v0) == STACK) {
+                    load_stack_to_vr(insn, v0, IR_VR_TYPE_U64);
+                }
+                if (vtype(*v1) == STACK) {
+                    load_stack_to_vr(insn, v1, IR_VR_TYPE_U64);
+                }
             } else if (insn->op == IR_INSN_ASSIGN) {
                 // dst = <val>
                 // MOV dst val
-
+                if (vtype_insn(insn) == STACK && vtype(*v0) == STACK) {
+                    load_stack_to_vr(insn, v0, IR_VR_TYPE_U64);
+                }
+            } else if (insn->op == IR_INSN_RET) {
+                // ret const/reg
+                // TODO: do this in explicit_reg?
+                // Load the result to R0
+                if (vtype(*v0) == STACK) {
+                    load_stack_to_vr(insn, v0, IR_VR_TYPE_U64);
+                }
             } else if (insn->op == IR_INSN_CALL) {
                 // call()
                 // Should have no arguments
                 DBGASSERT(insn->value_num == 0);
+            } else if (insn->op == IR_INSN_JA) {
+                // TODO
+            } else if (insn->op >= IR_INSN_JEQ && insn->op < IR_INSN_PHI) {
+                // TODO
             } else {
                 CRITICAL("No such instruction");
             }
