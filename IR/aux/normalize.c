399642e5b24b0358d47d0fa506ea9e3c6bd205f8
linsyking <kxiang@umich.edu>
2024-08-21 00:45:41 -0400

    feat: normalization done

--- IR/aux/normalize.c
+++ IR/aux/normalize.c
@@ -70,71 +70,17 @@ void normalize(struct ir_function *fun) {
                 // reg = const
                 // reg = stack
                 // reg = reg
-                if (tdst == STACK && t0 == STACK) {
-                    load_stack_to_r0(fun, insn, v0);
-                    res = 1;
-                }
-                // TODO: constant to stack: might need to first load to reg
+                CRITICAL("TODO");
             } else if (insn->op == IR_INSN_RET) {
-                // ret const/reg
-                // Done in explicit_reg pass
-                DBGASSERT(insn->value_num == 0);
+                // OK
             } else if (insn->op == IR_INSN_CALL) {
-                // call()
-                // Should have no arguments
-                DBGASSERT(insn->value_num == 0);
+                // OK
             } else if (insn->op == IR_INSN_JA) {
                 // OK
             } else if (insn->op >= IR_INSN_JEQ && insn->op < IR_INSN_PHI) {
                 // jeq reg const/reg
-                if ((t0 != REG && t1 == REG) || (t0 == CONST && t1 == STACK)) {
-                    struct ir_value tmp = *v0;
-                    *v0                 = *v1;
-                    *v1                 = tmp;
-                    enum val_type ttmp  = t0;
-                    t0                  = t1;
-                    t1                  = ttmp;
-                    // No need to spill here
-                }
-
-                if (t0 == REG) {
-                    // jeq reg reg ==> OK
-                    // jeq reg const ==> OK
-                    // jeq reg stack
-                    // ==>
-                    // reg2 = stack
-                    // jeq reg reg2
-                    if (t1 == STACK) {
-                        __u8            reg1     = insn_cg(v0->data.insn_d)->alloc_reg;
-                        __u8            reg2     = reg1 == 0 ? 1 : 0;
-                        struct ir_insn *new_insn = create_assign_insn_cg(insn, *v1, INSERT_FRONT);
-                        insn_cg(new_insn)->dst   = fun->cg_info.regs[reg2];
-                        v1->type                 = IR_VALUE_INSN;
-                        v1->data.insn_d          = fun->cg_info.regs[reg2];
-                        res                      = 1;
-                    }
-                } else {
-                    // jeq const1 const2
-                    // ==>
-                    // %tmp = const1
-                    // jeq %tmp const2
-                    if (t0 == CONST && t1 == CONST) {
-                        struct ir_insn *new_insn = create_assign_insn_cg(insn, *v0, INSERT_FRONT);
-                        v0->type                 = IR_VALUE_INSN;
-                        v0->data.insn_d          = new_insn;
-                        res                      = 1;
-                    }
-                    // jeq stack const
-                    if (t0 == STACK && t1 == CONST) {
-                        load_stack_to_r0(fun, insn, v0);
-                        res = 1;
-                    }
-                    // jeq stack stack
-                    if (t0 == STACK && t1 == STACK) {
-                        load_stack_to_r0(fun, insn, v0);
-                        res = 1;
-                    }
-                }
+                DBGASSERT(t0 == REG && (t1 == REG || t1 == CONST));
+                // OK
             } else {
                 CRITICAL("No such instruction");
             }
