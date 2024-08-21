c206b47637c8bbdaf4c00927364e88016671e3db
Mehbubul Hasan Al Quvi <57809679+quvi007@users.noreply.github.com>
2024-08-21 21:26:01 +0600

    Update prog_check.c

--- IR/aux/prog_check.c
+++ IR/aux/prog_check.c
@@ -87,16 +87,99 @@ void check_jumping(struct ir_function *fun) {
     array_for(pos, fun->reachable_bbs) {
         struct ir_basic_block *bb = *pos;
         struct ir_insn        *insn;
+        int jmp_exists = 0;
         list_for_each_entry(insn, &bb->ir_insn_head, list_ptr) {
-            if (insn->op == IR_INSN_JMP) {
+            if (is_jmp(insn)) {
+                jmp_exists = 1;
                 struct ir_insn *next_insn = list_next_entry(insn, list_ptr);
-                if (next_insn != NULL) {
+                if (next_insn != &bb->ir_insn_head) {
                     // Error
                     CRITICAL("Jump statement not at the end of a BB");
+                } else {
+                    // for conditional jumps, both BB1 and BB2 should be successors
+                    if (is_jmp_cond(insn)) {
+                        // get the two basic blocks that the conditional jump statement jumps to
+                        struct ir_basic_block *bb1 = insn->bb1;
+                        struct ir_basic_block *bb2 = insn->bb2;
+                        // check if the two basic blocks are successors of the current BB
+                        struct ir_basic_block **succ;
+                        int ok1 = 0;
+                        int ok2 = 0;
+                        array_for(succ, bb->succs) {
+                            struct ir_basic_block *succ_bb = *succ;
+                            if (succ_bb == bb1) {
+                                struct ir_basic_block *pred;
+                                array_for(pred, bb1->preds) {
+                                    struct ir_basic_block *pred_bb = *pred;
+                                    if (pred_bb == bb) {
+                                        ok1 = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (succ_bb == bb2) {
+                                struct ir_basic_block *pred;
+                                array_for(pred, bb2->preds) {
+                                    struct ir_basic_block *pred_bb = *pred;
+                                    if (pred_bb == bb) {
+                                        ok2 = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!ok1 || !ok2) {
+                            // Error
+                            CRITICAL("Conditional jump statement with operands that are not successors of the current BB");
+                        }
+                    } else {
+                        // for unconditional jumps, there should be only one successor
+                        if (bb->succs.num_elem != 1) {
+                            // Error
+                            CRITICAL("Unconditional jump statement with more than one successor");
+                        } else {
+                            // check if the jump operand is the only successor of BB
+                            struct ir_basic_block *succ = bb->succs.data[0];
+                            if (succ != insn->bb1) {
+                                // Error
+                                CRITICAL("ERR-Unconditional jump statement with a successor that is not the jump operand");
+                            }
+
+                            // check if the successor has the current BB as a predecessor
+                            // struct ir_basic_block **succ;
+                            // int found = 0;
+                            // array_for(succ, bb->succs) {
+                            //     struct ir_basic_block *succ_bb = *succ;
+                            //     struct ir_basic_block **pred;
+                            //     array_for(pred, succ_bb->preds) {
+                            //         struct ir_basic_block *pred_bb = *pred;
+                            //         if (pred_bb == bb) {
+                            //             found = 1;
+                            //             break;
+                            //         }
+                            //     }
+                            //     if (found) {
+                            //         break;
+                            //     }
+                            // }
+                            // if (!found) {
+                            //     // Error
+                            //     CRITICAL("Unconditional jump statement with a successor that does not have the current BB as a predecessor");
+                            // }
+                        }
+                    }
                 }
             }
         }
+        // if there is no jump statement, there should be only one successor
+        if (!jmp_exists) {
+            if (bb->succs.num_elem > 1) {
+                // Error
+                CRITICAL("No jump statement in BB but more than one successor");
+            }
+        }
     }
+    
 }
 
 // Check if the PHI nodes are at the beginning of the BB
