996aa0fe9460a764ccb4a8a099343cb6eb250cc0
linsyking <kxiang@umich.edu>
2024-08-25 18:32:26 -0400

    feat: error code

---
+++ IR/include/error.h
@@ -0,0 +1,20 @@
+#ifndef __BPF_IR_ERROR_H__
+#define __BPF_IR_ERROR_H__
+#include "bpf_ir.h"
+
+enum ir_error {
+    IR_ERROR_SYNTAX
+};
+
+struct error_code {
+    // Whether the function returns an error code
+    __u8 err : 1;
+
+    // Whether the error code is a kernel error code
+    __u8 ir_err : 1;
+
+    // The error code
+    int code : 30;
+};
+
+#endif
