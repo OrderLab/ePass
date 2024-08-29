53cecf23419bbd860f7b8f64777e815bf2976de6
linsyking <kxiang@umich.edu>
2024-08-29 11:17:29 -0400

    fix: prober

--- IR/probe.c
+++ IR/probe.c
@@ -4,15 +4,11 @@
 
 int bpf_ir_run(struct bpf_insn *insns, size_t len)
 {
-	printf("CLASS\tSIZE\tMODE\tOP\tSRC\tIMM\n");
-	for (size_t i = 0; i < len; ++i) {
-		__u8 code = insns[i].code;
-		// if (code == 0) {
-		//     continue;
-		// }
-		printf("%02x\t%02x\t%02x\t%02x\t%02x\t%d\n", BPF_CLASS(code),
-		       BPF_SIZE(code), BPF_MODE(code), BPF_OP(code),
-		       BPF_SRC(code), insns[i].imm);
+	for (__u32 i = 0; i <= len; ++i) {
+		struct bpf_insn *insn = &insns[i];
+		printf("insn[%d]: code=%d, dst_reg=%d, src_reg=%d, off=%d, imm=%d\n",
+		       i, insn->code, insn->dst_reg, insn->src_reg, insn->off,
+		       insn->imm);
 	}
 	return 0;
 }
