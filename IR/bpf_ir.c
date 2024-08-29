23836e674d100b7d68cc121ce560e8a2fe50edcb
linsyking <kxiang@umich.edu>
2024-08-28 21:58:15 -0400

    refactor: merge some passes to bpf_ir.c

--- IR/bpf_ir.c
+++ IR/bpf_ir.c
@@ -918,6 +918,72 @@ static int gen_function(struct ir_function *fun, struct ssa_transform_env *env)
 	return 0;
 }
 
+static void bpf_ir_fix_bb_succ(struct ir_function *fun)
+{
+	struct ir_basic_block **pos;
+	array_for(pos, fun->all_bbs)
+	{
+		struct ir_basic_block *bb = *pos;
+		struct ir_insn *insn = bpf_ir_get_last_insn(bb);
+		if (insn && is_cond_jmp(insn)) {
+			// Conditional jmp
+			if (bb->succs.num_elem != 2) {
+				CRITICAL(
+					"Conditional jmp with != 2 successors");
+			}
+			struct ir_basic_block **s1 = array_get(
+				&bb->succs, 0, struct ir_basic_block *);
+			struct ir_basic_block **s2 = array_get(
+				&bb->succs, 1, struct ir_basic_block *);
+			*s1 = insn->bb1;
+			*s2 = insn->bb2;
+		}
+	}
+}
+
+static void add_reach(struct ir_function *fun, struct ir_basic_block *bb)
+{
+	if (bb->_visited) {
+		return;
+	}
+	bb->_visited = 1;
+	bpf_ir_array_push(&fun->reachable_bbs, &bb);
+
+	struct ir_basic_block **succ;
+	__u8 i = 0;
+	array_for(succ, bb->succs)
+	{
+		if (i == 0) {
+			i = 1;
+			// Check if visited
+			if ((*succ)->_visited) {
+				CRITICAL("Loop BB detected");
+			}
+		}
+		add_reach(fun, *succ);
+	}
+}
+
+static void gen_reachable_bbs(struct ir_function *fun)
+{
+	clean_env(fun);
+	bpf_ir_array_clear(&fun->reachable_bbs);
+	add_reach(fun, fun->entry);
+}
+
+static void gen_end_bbs(struct ir_function *fun)
+{
+	struct ir_basic_block **pos;
+	bpf_ir_array_clear(&fun->end_bbs);
+	array_for(pos, fun->reachable_bbs)
+	{
+		struct ir_basic_block *bb = *pos;
+		if (bb->succs.num_elem == 0) {
+			bpf_ir_array_push(&fun->end_bbs, &bb);
+		}
+	}
+}
+
 static int run_passes(struct ir_function *fun)
 {
 	bpf_ir_prog_check(fun);
--- IR/include/bpf_ir.h
+++ IR/include/bpf_ir.h
@@ -454,10 +454,6 @@ struct ir_function {
 	struct code_gen_info cg_info;
 };
 
-// Constructor and Destructor
-
-void bpf_ir_fix_bb_succ(struct ir_function *fun);
-
 // IR checks
 
 void bpf_ir_prog_check(struct ir_function *fun);
@@ -690,10 +686,6 @@ void cut_bb(struct ir_function *fun);
 void add_counter(struct ir_function *fun);
 
 void add_constraint(struct ir_function *fun);
-
-void gen_reachable_bbs(struct ir_function *);
-
-void gen_end_bbs(struct ir_function *fun);
 
 struct function_pass {
 	void (*pass)(struct ir_function *);
