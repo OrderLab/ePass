8ff8d3a4588b4e232cfae131bfa24caab62693a5
linsyking <kxiang@umich.edu>
2024-09-12 10:58:30 -0400

    feat: add str and loop2

---
+++ IR/tests/str.c
@@ -0,0 +1,12 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int prog(void *ctx)
+{
+	char msg[] = "exit";
+	bpf_trace_printk(msg, sizeof(msg));
+	return XDP_DROP;
+}
+
+char _license[] SEC("license") = "GPL";
