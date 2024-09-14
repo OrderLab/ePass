1a0f24f486911b83995d9605eece61db2c520ca9
linsyking <kxiang@umich.edu>
2024-09-14 13:49:41 -0400

    feat: script

--- IR/scripts/gen_insn_ctor.py
+++ IR/scripts/gen_insn_ctor.py
@@ -7,24 +7,71 @@ import regex as re
 
 def handle_ir(matches, header, src):
     # print(matches)
-    for insn, args in matches:
-        print(insn)
+    for insn,extra, args in matches:
         args = args.split(",")
         args = [arg.strip() for arg in args]
-        if len(args) == 0:
-            print("Error: zero arguments")
+        if len(args) < 2:
+            print("Error: wrong arguments")
             continue
         if args[0] != "struct bpf_ir_env *env":
             print("Error: First argument is not struct bpf_ir_env *env")
             continue
-        print(args)
+        if args[1] != "struct ir_basic_block *bb":
+            print("Error: Second argument is not struct ir_basic_block *bb")
+            continue
+        del args[0]
+        del args[0]
+        # print(args)
+        cargs = []
+        rec = r".*[ \*](.*)$"
+        rec = re.compile(rec)
+        for arg in args:
+            res = rec.findall(arg)
+            cargs.append(res[0])
 
+        pargs = ", ".join(args)
+        cargs = ", ".join(cargs)
+        if len(pargs) > 0:
+            pargs = pargs + ","
+        if len(cargs) > 0:
+            cargs = "," +cargs 
+        ir_fun = f"""
+struct ir_insn *{insn}{extra}(struct bpf_ir_env *env, struct ir_insn *pos_insn, {pargs} enum insert_position pos)
+{{
+	struct ir_insn *new_insn =
+		{insn}_base{extra}(pos_insn->parent_bb {cargs});
+	insert_at(new_insn, pos_insn, pos);
+	return new_insn;
+}}
 
-def handle_cg(matches, header, src):
-    # print(matches)
-    for insn, args in matches:
-        print(insn)
+struct ir_insn *{insn}_bb{extra}(struct bpf_ir_env *env, struct ir_basic_block *pos_bb, {pargs} enum insert_position pos)
+{{
+	struct ir_insn *new_insn =
+		{insn}_base{extra}(pos_bb {cargs});
+	insert_at_bb(new_insn, pos_bb, pos);
+	return new_insn;
+}}
+"""
+        src.append(ir_fun)
 
+        ir_fun_h = f"""
+struct ir_insn *{insn}{extra}(struct bpf_ir_env *env, struct ir_insn *pos_insn, {pargs} enum insert_position pos);
+
+struct ir_insn *{insn}_bb{extra}(struct bpf_ir_env *env, struct ir_basic_block *pos_bb, {pargs} enum insert_position pos);
+"""
+        header.append(ir_fun_h)
+
+def insert(header, src):
+    srcfile = ""
+    with open("ir_insn.c") as f:
+        srcfile = f.read().split("/* Generated Constructors End */")
+    print(srcfile)
+    with open("ir_insn.c", "w") as f:
+        f.write(srcfile[0])
+        f.write("/* Generated Constructors Start */")
+        # f.write(header)
+        # f.write(src)
+        f.write("/* Generated Constructors End */")
 
 def main():
     header = []
@@ -32,14 +79,14 @@ def main():
     proto = ""
     with open("ir_insn.c") as f:
         proto = f.read()
-    regc = r"static struct ir_insn \*create_(.*?)_insn_base\(([\s\S]*?)\)"
+    regc = r"static struct ir_insn \*(create_.*?_insn)_base(.*?)\(([\s\S]*?)\)"
     regc = re.compile(regc)
     all_matches = regc.findall(proto)
     handle_ir(all_matches, header, src)
-    regc = r"static struct ir_insn \*create_(.*?)_insn_base_cg\(([\s\S]*?)\)"
-    regc = re.compile(regc)
-    all_matches = regc.findall(proto)
-    handle_cg(all_matches, header, src)
+    header = "".join(header)
+    src = "".join(src)
+    # print(src)
+    insert(header, src)
 
 
 main()
