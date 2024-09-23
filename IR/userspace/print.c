1a754caa64a5d8ebc682a8f1f5d97a4bf3b54921
linsyking <kxiang@umich.edu>
2024-09-23 00:20:43 -0400

    feat: better printer

--- IR/userspace/print.c
+++ IR/userspace/print.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "bpf/libbpf.h"
+#include <linux/bpf_ir.h>
 
-int print(const struct bpf_insn *insns, size_t len)
+static void print_bpf_prog(struct bpf_ir_env *env, const struct bpf_insn *insns,
+			   size_t len)
 {
 	for (size_t i = 0; i < len; ++i) {
 		const struct bpf_insn *insn = &insns[i];
-		// printf("insn[%d]: code=%x, dst_reg=%x, src_reg=%x, off=%x, imm=%x\n",
-		//        i, insn->code, insn->dst_reg, insn->src_reg, insn->off,
-		//        insn->imm);
-		__u64 data;
-		memcpy(&data, insn, sizeof(struct bpf_insn));
-		printf("insn[%zu]: %llu\n", i, data);
+		if (insn->code == 0) {
+			continue;
+		}
+		PRINT_LOG(env, "[%zu] ", i);
+		bpf_ir_print_bpf_insn(env, insn);
 	}
-	return 0;
 }
 
 int main(int argn, char **argv)
@@ -29,6 +29,16 @@ int main(int argn, char **argv)
 	size_t sz = bpf_program__insn_cnt(prog);
 	const struct bpf_insn *insn = bpf_program__insns(prog);
 	// bpf_program__set_insns
-	print(insn, sz);
+	struct bpf_ir_opts opts = {
+		.debug = 1,
+		.print_mode = BPF_IR_PRINT_BPF,
+	};
+	struct bpf_ir_env *env = bpf_ir_init_env(opts);
+	if (!env) {
+		return 1;
+	}
+	print_bpf_prog(env, insn, sz);
+	bpf_ir_print_log_dbg(env);
+	bpf_ir_free_env(env);
 	bpf_object__close(obj);
 }
