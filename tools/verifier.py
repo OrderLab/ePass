3bb9daf92798f2242b08a5ec984083c45c68d9be
linsyking <kxiang@umich.edu>
2024-09-22 22:40:36 -0400

    feat: add header

--- tools/verifier.py
+++ tools/verifier.py
@@ -3,13 +3,17 @@
 import regex as re
 
 gid = 0
+gmap = dict()
+
 
 def fun(match):
-    global gid
+    global gid, gmap
     old = match.group()
     newitem = f"verbose_err({gid}, "
+    gmap[gid] = match.group(1)
     gid += 1
     return old.replace("verbose(", newitem)
+
 
 content = ""
 with open("verifier.c", "r") as f:
@@ -22,3 +26,19 @@ nc = re.sub(rec, fun, content)
 
 with open("verifier_modified.c", "w") as f:
     f.write(nc)
+
+data = ""
+for i in gmap:
+    comment = gmap[i]
+    if comment[-2:] == "\\n":
+        comment = comment[:-2]
+    data += f"BPF_VERIFIER_ERR_{i} = {i}, // {comment} \n"
+
+header = f"""
+enum bpf_verifier_error{{
+    {data}
+}};
+"""
+
+with open("error.h", "w") as f:
+    f.write(header)
