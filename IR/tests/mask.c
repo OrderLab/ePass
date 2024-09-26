ef79e7ccae8adf07fb7f96a171dd94479c4e67b2
linsyking <kxiang@umich.edu>
2024-09-25 22:23:50 -0400

    feat: change mask test

--- IR/tests/mask.c
+++ IR/tests/mask.c
@@ -1,20 +1,20 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-#define SIZE 100
+#define SIZE 123
 
 SEC("xdp")
 int prog(void *ctx)
 {
 	int t = bpf_ktime_get_ns() % SIZE;
-	static int a[SIZE] = { 0 };
+	static short a[SIZE] = { 0 };
 	for (int i = 0; i < SIZE; ++i) {
 		a[i] = i;
 	}
 	static char msg[] = "num:  %d";
-	if (t < 0 || t > 100) {
-		return 0;
-	}
+	// if (t < 0 || t > 100) {
+	// 	return 0;
+	// }
 	bpf_trace_printk(msg, sizeof(msg), a[t]);
 	return 0;
 }
