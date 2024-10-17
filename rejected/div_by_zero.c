4ecf090b3778fe693c6a40db50364019ee13d158
Mehbubul Hasan Al Quvi <57809679+quvi007@users.noreply.github.com>
2024-10-17 22:45:47 +0600

    fix div_by_zero.c

--- rejected/div_by_zero.c
+++ rejected/div_by_zero.c
@@ -4,24 +4,23 @@
 SEC("tracepoint/syscalls/sys_enter_execve")
 int handle_tp(void *ctx)
 {
-    int x = 0;
-    for (int i = 0; i <= 10; i++) {
-        for (int j = 0; j <= 10; j++) {
-            if (i > 10) {
-                i /= x;
-                return 0;
-            }
-            if (j > 10) {
-                i /= x;
-                return 0;
-            }
-            i *= j;
-            if (i != 11) {
-                i /= x;
-                return 0;
-            }
-        }
-    }
+    int r0 = 0;
+    int r1 = 10;
+    int r2 = 10;
+
+    if (r1 > 10)
+        return 0; // r1 is in [0, 10]
+    
+    if (r2 > 10)
+        return 0; // r2 is in [0, 10]
+
+    r1 *= r2; // r1 is now in [0, 100]
+
+    if (r1 != 11) // Checking if r1 is 11, but r1 can't be 11
+        return 0;
+    
+    r1 /= r0; // Potential division by zero
+
     return 0;
 }
 
