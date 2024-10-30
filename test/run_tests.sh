9c5007fe8b8129a98e8cb83fa88f50a2589cb9c9
linsyking <kxiang@umich.edu>
2024-10-29 22:54:13 -0400

    feat: add some pass programs

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-files=$(find output -iname '*.o')
+# files=$(find output -iname '*.o')
+
+# for file in $files; do
+#     printf "Testing $file ..."
+#     epasstool -m read -p $file -s "prog"
+#     retVal=$?
+#     if [ $retVal -ne 0 ]; then
+#         echo "FAILED"
+#         continue
+#     fi
+#     echo "PASSED"
+# done
+
+# Test logs, may not pass (in most cases)
+# files=$(find progs/txt/fail -iname '*.txt')
+# for file in $files; do
+#     printf "Testing $file ..."
+#     epasstool -m readlog -p $file > /dev/null
+#     echo "PASSED"
+# done
+
+files=$(find progs/txt/pass -iname '*.txt')
 
 for file in $files; do
     printf "Testing $file ..."
-    epasstool -m read -p $file -s "prog"
+    epasstool -m readlog -p $file -s "prog"
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "FAILED"
         continue
     fi
-    echo "PASSED"
-done
-
-# Test logs, may not pass (in most cases)
-files=$(find progs -iname '*.txt')
-for file in $files; do
-    printf "Testing $file ..."
-    epasstool -m readlog -p $file > /dev/null
     echo "PASSED"
 done
