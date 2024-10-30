ec6bc51ed433fca27f7ccf1d9ff0f2ff4868ce6c
linsyking <kxiang@umich.edu>
2024-10-30 02:04:45 -0400

    feat: alloc zero mapping space

--- core/passes/msan.c
+++ core/passes/msan.c
@@ -6,9 +6,12 @@ void msan(struct bpf_ir_env *env, struct ir_function *fun, void *param)
 	// Add the 64B mapping space
 	struct ir_insn *arr = bpf_ir_create_allocarray_insn_bb(
 		env, fun->entry, IR_VR_TYPE_64, 8, INSERT_FRONT_AFTER_PHI);
-	bpf_ir_create_storeraw_insn(env, arr, IR_VR_TYPE_64,
-				    bpf_ir_addr_val(bpf_ir_value_insn(arr), 0),
-				    bpf_ir_value_const32(0), INSERT_BACK);
+	for (int i = 0; i < 8; ++i) {
+		bpf_ir_create_storeraw_insn(
+			env, arr, IR_VR_TYPE_64,
+			bpf_ir_addr_val(bpf_ir_value_insn(arr), i * 8),
+			bpf_ir_value_const32(0), INSERT_BACK);
+	}
 }
 
 const struct builtin_pass_cfg bpf_ir_kern_msan =
