e064226577aa78374da3707eb0ed5e9236660db0
Linsy King <linsy@linux.localdomain>
2024-11-11 23:40:17 -0500

    feat: evaluate

--- test/evaluate.py
+++ test/evaluate.py
@@ -4,3 +4,9 @@ Run the program w/ and w/o ePass
 Evaluate the time it costs
 """
 
+import os
+
+def init():
+    print("init...")
+    os.system("sudo /sbin/sysctl -w kernel.bpf_stats_enabled=1")
+
