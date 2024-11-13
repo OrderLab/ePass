ac08155632eaa388eee6abbdfe357b1e741f881d
linsyking <kxiang@umich.edu>
2024-11-12 20:11:31 -0500

    feat: add tests

--- test/gen_tests.sh
+++ test/gen_tests.sh
@@ -6,8 +6,14 @@ mkdir -p output
 
 files=$(find . -iname '*.c')
 
+rm Makefile
+all_objs=""
+
 for file in $files; do
     base_name=$(echo $file | sed -r 's/\.c//g' | sed -r 's/\.\///g' | tr '/' '_')
-    clang -O2 -I./progs -I/usr/include/$(uname -m)-linux-gnu -D__TARGET_ARCH_x86 -target bpf -g -c $file -o output/$base_name.o
-    clang -O2 -I./progs -I/usr/include/$(uname -m)-linux-gnu -D__TARGET_ARCH_x86 -target bpf -g -emit-llvm -S -c $file -o output/$base_name.ll
+    printf "output/$base_name.o: $file\n\tclang -O2 -I./progs -I/usr/include/$(uname -m)-linux-gnu -D__TARGET_ARCH_x86 -target bpf -g -c \$< -o \$@\n" >> Makefile
+    printf "output/$base_name.ll: $file\n\tclang -O2 -I./progs -I/usr/include/$(uname -m)-linux-gnu -D__TARGET_ARCH_x86 -target bpf -g -emit-llvm -S -c \$< -o \$@\n" >> Makefile
+    all_objs="output/$base_name.o output/$base_name.ll "$all_objs
 done
+
+echo "all:$all_objs" >> Makefile
