1c994251071de7ee9d427734f20e9f5e12d111f7
yimingx <yimingx@node0.yimingx-233609.failuredetection-pg0.apt.emulab.net>
2024-11-25 19:00:36 -0700

    fix: remove test programs

--- test/progs/latency.c
+++ test/progs/latency.c
@@ -7,7 +7,7 @@ int prog(void *ctx)
     __u64 start_time = bpf_ktime_get_ns(); // Get start time
 
     // Simulate some processing here
-    bpf_trace_printk("Processing...\n");
+    bpf_printk("Processing...\n");
 
     __u64 end_time = bpf_ktime_get_ns(); // Get end time
     __u64 latency = end_time - start_time; // Calculate latency
