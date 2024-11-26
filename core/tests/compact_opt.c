eed55fc568e8fb58b6ad12df27fd03f6e315d157
Yiming Xiang <kxiang@umich.edu>
2024-11-25 21:20:28 -0500

    feat: add test

---
+++ core/tests/compact_opt.c
@@ -0,0 +1,13 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int prog(void *ctx)
+{
+    __u64 x = 2;
+    __u64 x2 = (x <<32) >>32;
+    bpf_printk("hello %d", x2);
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
