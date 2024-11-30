50b0b709aa4eae05bcd9b74994c59618bf93f7c8
linsyking <kxiang@umich.edu>
2024-11-30 11:26:26 -0500

    feat: add tests

---
+++ core/tests/1mloop.c
@@ -0,0 +1,26 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("tp/syscalls/sys_enter_execve")
+int prog(void *ctx) {
+    __u64 t = bpf_ktime_get_ns();
+    for (__u64 i = 0; i < t; ++i) {
+		if (i > 100000) {
+			return 1;
+		}
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+        bpf_ktime_get_ns();
+    }
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
