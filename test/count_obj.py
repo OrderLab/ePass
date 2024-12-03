ffa3fbd24795963ca1432e1e16f8f3679c094505
linsyking <kxiang@umich.edu>
2024-12-03 01:34:33 -0500

    feat: eval obj

--- test/count_obj.py
+++ test/count_obj.py
@@ -2,6 +2,30 @@ import re
 import glob
 import os
 
-o_files = glob.glob(os.path.join("output", "*.o"))
+o_files = glob.glob(os.path.join("output", "*.s"))
+nums = []
+for f in o_files:
+    with open(f, "r") as file:
+        content = file.read()
+    count = re.findall(r" ([0-9]*?):", content)
+    maxn = 0
+    for c in count:
+        if c != "" and int(c) > maxn:
+            maxn = int(c)
+    nums.append(maxn+1)
 
-print("Number of object files: ", len(o_files))
+print(nums)
+
+o_files = glob.glob(os.path.join("progs/txt", "*.txt"))
+nums = []
+for f in o_files:
+    with open(f, "r") as file:
+        content = file.read()
+    count = re.findall(r"insn\[([0-9]*?)\]", content)
+    maxn = 0
+    for c in count:
+        if c != "" and int(c) > maxn:
+            maxn = int(c)
+    nums.append(maxn+1)
+
+print(max(nums), min(nums), sum(nums)/len(nums))
