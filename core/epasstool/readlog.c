57a4bc2eacac622c1acb0e76b890d58c44b1464f
linsyking <kxiang@umich.edu>
2025-04-12 02:03:06 -0400

    feat: read log mem

--- core/epasstool/readlog.c
+++ core/epasstool/readlog.c
@@ -15,20 +15,27 @@ int epass_readlog(struct user_opts uopts)
 		return 1;
 	}
 	char line[256];
-	struct bpf_insn *insns = malloc_proto(sizeof(struct bpf_insn) * 5000);
+	struct array insns_array;
+	struct bpf_ir_env tenv; // tmp
+	INIT_ARRAY(&insns_array, struct bpf_insn);
+	// struct bpf_insn *insns = malloc_proto(sizeof(struct bpf_insn) * 5000);
 	size_t index = 0;
 	while (fgets(line, sizeof(line), fp)) {
 		u64 s = strtoull(line, NULL, 10);
 		if (line[0] == '\n') {
 			break;
 		}
-		memcpy(&insns[index], &s, sizeof(struct bpf_insn));
+		struct bpf_insn tmp;
+		memcpy(&tmp, &s, sizeof(struct bpf_insn));
+		bpf_ir_array_push(&tenv, &insns_array, &tmp);
 		index++;
 	}
 
-	err = epass_run(uopts, insns, index);
+	err = epass_run(uopts, insns_array.data, index);
 
 end:
+	bpf_ir_array_free(&insns_array);
+
 	fclose(fp);
 	return err;
 }
