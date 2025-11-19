ae2a3328f585035416eef5f8888dcce099a2dc30
Yiming Xiang <yiming@cs.utexas.edu>
2025-11-19 19:18:48 +0000

    feat: vm start script

--- tools/startvm.sh
+++ tools/startvm.sh
@@ -6,6 +6,8 @@ KERNEL_PATH=$GIT_REPO/ePass-kernel
 
 DRIVE_PATH=$GIT_REPO/test/vm/disk.qcow2
 
-# screen -S epass -dm bash -c "qemu-system-x86_64 -enable-kvm -drive file=${DRIVE_PATH},format=qcow2 -kernel ${KERNEL_PATH}/arch/x86_64/boot/bzImage -append 'root=/dev/sda1' --nographic -net nic -net user,hostfwd=tcp::2222-:22 -cpu host -smp 16 -m 8G"
-qemu-system-x86_64 -enable-kvm -drive file=${DRIVE_PATH},format=qcow2 --nographic -net nic -net user,hostfwd=tcp::2222-:22 -cpu host -smp 16 -m 8G
-# qemu-system-x86_64 -enable-kvm -drive file=${DRIVE_PATH},format=qcow2 -kernel ${KERNEL_PATH}/arch/x86_64/boot/bzImage -append 'root=/dev/sda1' --nographic -net nic -net user,hostfwd=tcp::2222-:22 -cpu host -smp 16 -m 8G
+screen -S epass -dm bash -c "qemu-system-x86_64 -enable-kvm -drive file=${DRIVE_PATH},format=qcow2 -kernel ${KERNEL_PATH}/arch/x86_64/boot/bzImage -append 'root=/dev/sda1' --nographic -net nic -net user,hostfwd=tcp::2222-:22 -cpu host -smp 16 -m 8G"
+
+# To start with default kernel, run:
+
+# qemu-system-x86_64 -enable-kvm -drive file=${DRIVE_PATH},format=qcow2 --nographic -net nic -net user,hostfwd=tcp::2222-:22 -cpu host -smp 16 -m 8G
