13168d62dec83dff38c1115cd1dee539976b1f76
Yiming Xiang <yiming@cs.utexas.edu>
2025-11-25 23:13:23 +0000

    feat: malloc API

--- core/bpftests/asm.c
+++ core/bpftests/asm.c
@@ -1,20 +1,58 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-static __always_inline long emit_ec(long x)
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, char[1024]);
+} meta SEC(".maps");
+
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, char[1024]);
+} data SEC(".maps");
+
+static __always_inline void *malloc(void *x, void *y, long size)
 {
-	register long r1 asm("r1") = x; // src = r1
-	register long r0 asm("r0"); // dst = r0
-	asm volatile(".byte 0x85, 0x61, 0,0,0,0,0,0\n" : "=r"(r0) : "r"(r1) :);
-	return r0;
+	register long r1 asm("r1") = (long)x;
+	register long r2 asm("r2") = (long)y;
+	register long r3 asm("r3") = size;
+	register long r0 asm("r0");
+	asm volatile(".byte 0x85, 0x63, 0,0,0,0,0,0\n"
+		     : "=r"(r0)
+		     : "r"(r1), "r"(r2), "r"(r3)
+		     :);
+	return (void *)r0;
 }
+
+
+static __always_inline void free(void *x)
+{
+	register long r1 asm("r1") = (long)x;
+	asm volatile(".byte 0x85, 0x61, 0,0,1,0,0,0\n"
+		     : 
+		     : "r"(r1)
+		     :);
+	return;
+}
+
+struct test_struct {
+	int a;
+	long b;
+};
 
 SEC("xdp")
 int prog(void *ctx)
 {
-	long xx = 123;
-	long bb = emit_ec(xx);
-	bpf_printk("ecall returned: %ld\n", bb);
+	struct test_struct *bb = (struct test_struct*) malloc(&meta, &data, sizeof(struct test_struct));
+	bb->a = 42;
+	bb->b = 123456789;
+	bpf_printk("ecall returned: %d\n", bb->a);
+	free(bb);
 	return 0;
 }
 
--- core/passes/ecall.c
+++ core/passes/ecall.c
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include "ir.h"
+#include "linux/bpf_ir.h"
+
+void translate_malloc(struct bpf_ir_env *env, struct ir_function *fun)
+{
+}
 
 /**
  * bpf_ir_handle_ecalls - Handle ecall instructions in the IR
@@ -11,4 +16,15 @@
 void bpf_ir_handle_ecalls(struct bpf_ir_env *env, struct ir_function *fun,
 			  void *param)
 {
+	struct ir_basic_block **pos;
+	array_for(pos, fun->reachable_bbs)
+	{
+		struct ir_basic_block *bb = *pos;
+		struct ir_insn *insn;
+		list_for_each_entry(insn, &bb->ir_insn_head, list_ptr) {
+			if (insn->op == IR_INSN_ECALL) {
+				bpf_ir_erase_insn(env, insn);
+			}
+		}
+	}
 }
